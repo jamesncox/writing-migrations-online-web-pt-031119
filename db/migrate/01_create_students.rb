9acdc662e475bb3d787e28ff3202cfc91b99bf6c
James Cox <jamesncox@yahoo.com>
2019-05-28 17:32:46 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -4,15 +4,4 @@ class CreateStudents < ActiveRecord::Migration[5.1]
       t.string :name
     end
   end
-  
-  class CreateArtists < ActiveRecord::Migration[4.2]
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
-  end
-end
 end
